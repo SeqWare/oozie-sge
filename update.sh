4371ffae9450d200c512e03f51bcf1cd47f407ac
Alexander Taggart <Alexander.Taggart@oicr.on.ca>
2013-05-09 10:17:16 -0400

    Only push workflow.xml to HDFS, instead of entire example dir.

--- update.sh
+++ update.sh
@@ -46,7 +46,8 @@ $SSH -t "chmod -R 777 "$NFS_EXAMPLE
 echo
 echo "Updating hadoop:"
 $SSH -t "sudo -u oozie hadoop fs -rm -r "$HDFS_EXAMPLE" ;\
-         sudo -u oozie hadoop fs -put "$NFS_EXAMPLE" "$HDFS_ROOT
+         sudo -u oozie hadoop fs -mkdir "$HDFS_EXAMPLE" ; \
+         sudo -u oozie hadoop fs -put "$NFS_EXAMPLE"/workflow.xml "$HDFS_EXAMPLE"/workflow.xml"
 
 echo
 echo "Done!"
