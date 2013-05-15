676ca8091b78c6bd48418c87ce10da8cb45439bb
Alexander Taggart <Alexander.Taggart@oicr.on.ca>
2013-05-15 08:57:25 -0400

    Clean up tests.

--- src/test/java/org/apache/oozie/action/sge/QacctTest.java
+++ src/test/java/org/apache/oozie/action/sge/QacctTest.java
@@ -1,100 +1,24 @@
 package org.apache.oozie.action.sge;
 
+import java.io.File;
 import java.util.Map;
 
 import junit.framework.TestCase;
 
 public class QacctTest extends TestCase {
-
-  private static final String good = 
-      "==============================================================\n" + 
-  		"qname        all.q               \n" + 
-  		"hostname     master              \n" + 
-  		"group        oozie               \n" + 
-  		"owner        oozie               \n" + 
-  		"project      NONE                \n" + 
-  		"department   defaultdepartment   \n" + 
-  		"jobname      task.sh             \n" + 
-  		"jobnumber    11687               \n" + 
-  		"taskid       undefined\n" + 
-  		"account      sge                 \n" + 
-  		"priority     0                   \n" + 
-  		"qsub_time    Tue May  7 10:19:08 2013\n" + 
-  		"start_time   Tue May  7 10:19:12 2013\n" + 
-  		"end_time     Tue May  7 10:19:22 2013\n" + 
-  		"granted_pe   NONE                \n" + 
-  		"slots        1                   \n" + 
-  		"failed       0    \n" + 
-  		"exit_status  0                   \n" + 
-  		"ru_wallclock 10           \n" + 
-  		"ru_utime     0.036        \n";
-  
-  private static final String failed = 
-      "==============================================================\n" + 
-      "qname        all.q               \n" + 
-      "hostname     master              \n" + 
-      "group        oozie               \n" + 
-      "owner        oozie               \n" + 
-      "project      NONE                \n" + 
-      "department   defaultdepartment   \n" + 
-      "jobname      task.sh             \n" + 
-      "jobnumber    11687               \n" + 
-      "taskid       undefined\n" + 
-      "account      sge                 \n" + 
-      "priority     0                   \n" + 
-      "qsub_time    Tue May  7 10:19:08 2013\n" + 
-      "start_time   Tue May  7 10:19:12 2013\n" + 
-      "end_time     Tue May  7 10:19:22 2013\n" + 
-      "granted_pe   NONE                \n" + 
-      "slots        1                   \n" + 
-      "failed       1    \n" + 
-      "exit_status  0                   \n" + 
-      "ru_wallclock 10           \n" + 
-      "ru_utime     0.036        \n";
-  
-  private static final String badExit = 
-      "==============================================================\n" + 
-      "qname        all.q               \n" + 
-      "hostname     master              \n" + 
-      "group        oozie               \n" + 
-      "owner        oozie               \n" + 
-      "project      NONE                \n" + 
-      "department   defaultdepartment   \n" + 
-      "jobname      task.sh             \n" + 
-      "jobnumber    11687               \n" + 
-      "taskid       undefined\n" + 
-      "account      sge                 \n" + 
-      "priority     0                   \n" + 
-      "qsub_time    Tue May  7 10:19:08 2013\n" + 
-      "start_time   Tue May  7 10:19:12 2013\n" + 
-      "end_time     Tue May  7 10:19:22 2013\n" + 
-      "granted_pe   NONE                \n" + 
-      "slots        1                   \n" + 
-      "failed       0    \n" + 
-      "exit_status  1                   \n" + 
-      "ru_wallclock 10           \n" + 
-      "ru_utime     0.036        \n";
-  
-  public void testToMap() throws Exception {
-
-    Map<String, String> result =  Qacct.toMap(good);
-    assertNotNull(result);
-    
-    assertEquals("0", result.get("failed"));
-    assertEquals("0", result.get("exit_status"));
-  }
   
   public void testChecks(){
-    Map<String, String> result =  Qacct.toMap(good);
+    Map<String, String> result;
+    
+    result = Qacct.done(new File("src/test/bin/qacct-ok").getAbsolutePath(), "12345");
     assertFalse(Qacct.failed(result));
     assertFalse(Qacct.exitError(result));
     
-    result =  Qacct.toMap(failed);
+    result = Qacct.done(new File("src/test/bin/qacct-failed").getAbsolutePath(), "12345");
     assertTrue(Qacct.failed(result));
     assertFalse(Qacct.exitError(result));
     
-    
-    result =  Qacct.toMap(badExit);
+    result = Qacct.done(new File("src/test/bin/qacct-exit-error").getAbsolutePath(), "12345");
     assertFalse(Qacct.failed(result));
     assertTrue(Qacct.exitError(result));
   }
--- src/test/java/org/apache/oozie/action/sge/QsubTest.java
+++ src/test/java/org/apache/oozie/action/sge/QsubTest.java
@@ -7,12 +7,10 @@ import junit.framework.TestCase;
 public class QsubTest extends TestCase {
 
   public void testInvoke() throws Exception {
-
+    String jobId = Qsub.invoke(new File("src/test/bin/qsub-ok").getAbsolutePath(),
+                               new File("task.sh"),
+                               new File("src/test/bin"), null);
     assertEquals("Did not obtain a JobId. Ensure testing version of qsub is used.",
-                 "foobar123", Qsub
-                     .invoke(new File("src/test/bin/qsub-ok").getAbsolutePath(),
-                             new File("src/test/bin/some-script.sh"),
-                             new File("src/test/bin"), null));
-
+                 "foobar123", jobId);
   }
 }
