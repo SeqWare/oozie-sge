bf4352c61a7d33e65595d67fda915e47d4aa7fc1
Alexander Taggart <Alexander.Taggart@oicr.on.ca>
2013-05-28 12:58:52 -0400

    Update qsub/qstat tests

--- src/test/java/org/apache/oozie/action/sge/QsubTest.java
+++ src/test/java/org/apache/oozie/action/sge/QsubTest.java
@@ -11,11 +11,11 @@ public class QsubTest extends TestCase {
     String asUser = null;
     File options = null;
     Map<Object, Object> env = null;
-    String jobId = Qsub.invoke(new File("src/test/bin/qsub-ok").getAbsolutePath(),
-                               asUser,
-                               new File("src/test/bin/task.sh").getAbsoluteFile(),
-                               options, env);
+    Result result = Qsub.invoke(new File("src/test/bin/qsub-ok").getAbsolutePath(),
+                                asUser,
+                                new File("src/test/bin/task.sh").getAbsoluteFile(),
+                                options, env);
     assertEquals("Did not obtain a JobId. Ensure testing version of qsub is used.",
-                 "foobar123", jobId);
+                 "foobar123", Qsub.getJobId(result));
   }
 }
