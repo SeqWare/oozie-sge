facd11efd5a70cbe3ccb1751a1bf67e84a0507d0
Denis Yuen <dyuen@oicr.on.ca>
2014-03-14 14:20:19 -0400

    Remove test with sudo for jenkins

--- src/test/java/io/seqware/oozie/action/sge/QdelTest.java
+++ src/test/java/io/seqware/oozie/action/sge/QdelTest.java
@@ -9,10 +9,7 @@ import junit.framework.TestCase;
 public class QdelTest extends TestCase {
 
   public void testInvoke() throws Exception {
-    String asUser = "dyuen";
-    Result result = Qdel.invoke(new File("src/test/bin/qdel-ok").getAbsolutePath(), asUser, "234");
-    assertEquals("user has registered the job 234 for deletion", result.output.trim());
-    result = Qdel.invoke(new File("src/test/bin/qdel-ok").getAbsolutePath(), null, "234");
+    Result result = Qdel.invoke(new File("src/test/bin/qdel-ok").getAbsolutePath(), null, "234");
     assertEquals("user has registered the job 234 for deletion", result.output.trim());
   }
 }
